3ff7b58db9c027c50b894a86ab6e59665bd261b4
luismedinaeng <malf95@hotmail.com>
2020-06-10 18:17:10 -0500

    [Update]Intialize bets list and add a RouletteId

--- Models/Roulette.cs
+++ Models/Roulette.cs
@@ -13,11 +13,13 @@ namespace RouletteApi.Models
         {
             this.Status = Constants.CLOSE;
             this.Result = Constants.WITHOUT_RESULT;
+            Bets = new List<Bet>();
         }
         public bool AddBet(Bet newBet)
         {
             if (string.Compare(this.Status, Constants.OPEN) == 0)
             {
+                newBet.RouletteId = this.Id;
                 Bets.Add(newBet);
                 return true;
             }
