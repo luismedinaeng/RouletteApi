6ba5c1221f87fba51cd1f5066f151471f47c88fa
luismedinaeng <malf95@hotmail.com>
2020-06-10 12:19:15 -0500

    [Add]Roulette class

---
+++ Models/Roulette.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+
+namespace RouletteApi.Models
+{   
+    public class Roulette
+    {
+        public long Id { get; set; }
+        public string Status { get; set; }
+        public int Result { get; set; }
+        public virtual ICollection<Bet> Bets { get; set; }
+
+        public Roulette()
+        {
+            this.Status = Constants.CLOSE;
+            this.Result = Constants.WITHOUT_RESULT;
+        }
+        public bool addBet(Bet newBet)
+        {
+            if (string.Compare(this.Status, Constants.OPEN) == 0)
+            {
+                Bets.Add(newBet);
+                return true;
+            }
+            else
+                return false;
+        }
+
+    }
+}
