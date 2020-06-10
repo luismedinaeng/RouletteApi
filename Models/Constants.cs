83c4ca35350d17ebc41b7119b4a2a56acc97c34a
luismedinaeng <malf95@hotmail.com>
2020-06-09 21:48:09 -0500

    [Add]Constants

---
+++ Models/Constants.cs
@@ -0,0 +1,15 @@
+namespace RouletteApi.Models
+{
+    public static class Constants
+    {
+        public const int BLACK_TOKEN = -1;
+        public const int RED_TOKEN = -2;
+        public const int MIN_TOKEN = 0;
+        public const int MAX_TOKEN = 36;
+        public const long MAX_VALUE = 10000;
+        public const int WAITING = 0;
+        public const int WIN = 1;
+        public const int LOSE = 2;
+
+    }
+}
