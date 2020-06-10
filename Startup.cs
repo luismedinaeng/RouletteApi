4fc09e870c3ca8cb4751c0b385b37198579e57ac
luismedinaeng <malf95@hotmail.com>
2020-06-10 12:11:24 -0500

    [Delete]Unused usigns & [Add]DbContext

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using RouletteApi.Models;
 
 namespace RouletteApi
 {
@@ -25,6 +20,8 @@ namespace RouletteApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RouletteContext>(opt =>
+               opt.UseInMemoryDatabase("RouletteList"));
             services.AddControllers();
         }
 
