cf1843674a7c218e6cfc710e10caaa8b9f5a7e6b
luismedinaeng <malf95@hotmail.com>
2020-06-10 12:07:10 -0500

    [add]Constructor of class

--- Models/Bet.cs
+++ Models/Bet.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace RouletteApi.Models
 {
     public class Bet
@@ -9,6 +11,20 @@ namespace RouletteApi.Models
         public string UserId { get; set; }
         public long RouletteId { get; set; }
         
+        public Bet(int token, long value, string userId, long rouletteId)
+        {
+            if ((token == Constants.BLACK_TOKEN || token == Constants.RED_TOKEN) || (token >= Constants.MIN_TOKEN && token <= Constants.MAX_TOKEN))
+                this.Token = token;
+            else
+                throw new ArgumentOutOfRangeException("token");
+            if (value > 0 || value <= Constants.MAX_VALUE)
+                this.Value = value;
+            else
+                throw new ArgumentOutOfRangeException("value");
+            this.UserId = userId;
+            this.RouletteId = rouletteId;
+        }
+
         public void updateStatus(int resultNumber, int resultColor)
         {
             if (this.Token == resultColor || this.Token == resultNumber)
