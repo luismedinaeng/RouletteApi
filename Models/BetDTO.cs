36cd75657b69f443ea48280e581e9a87df365c63
luismedinaeng <malf95@hotmail.com>
2020-06-10 18:25:34 -0500

    [Add]Data Transfer object for bet

---
+++ Models/BetDTO.cs
@@ -0,0 +1,9 @@
+namespace RouletteApi.Models
+{
+    public class BetDTO
+    {
+        public int Token { get; set; }
+        public long Value { get; set; }
+        public int Status { get; set; }
+    }
+}
