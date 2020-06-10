be4818de890954715958b58e5b8f1b14911aa02a
luismedinaeng <malf95@hotmail.com>
2020-06-09 21:47:53 -0500

    [Update]Add updateStatus method

--- Models/Bet.cs
+++ Models/Bet.cs
@@ -1,12 +1,21 @@
 namespace RouletteApi.Models
 {
-    public class Bet {
+    public class Bet
+    {    
         public long Id { get; set; }
-        public string Token { get; set; }
+        public int Token { get; set; }
         public long Value { get; set; }
         public int Status { get; set; }
         public string UserId { get; set; }
         public long RouletteId { get; set; }
         
+        public void updateStatus(int resultNumber, int resultColor)
+        {
+            if (this.Token == resultColor || this.Token == resultNumber)
+                this.Status = Constants.WIN;
+            else
+                this.Status = Constants.LOSE;
+        }
+
     }
 }
