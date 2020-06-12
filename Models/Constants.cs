fa24328141e98c25e762db53e3344854135f2ca0
luismedinaeng <malf95@hotmail.com>
2020-06-12 16:04:33 -0500

    [Add]Function to determine the color of the actual bet

--- Models/Constants.cs
+++ Models/Constants.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace RouletteApi.Models
 {
     public static class Constants
@@ -12,7 +14,15 @@ namespace RouletteApi.Models
         public const int LOSE = 2;
         public const string OPEN = "open";
         public const string CLOSE = "close";
-
         public const int WITHOUT_RESULT = -1;
+
+        public static int GetTokenColor(int token)
+        {
+            List<int> black_tokens = new List<int>() {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
+            if (black_tokens.Contains(token))
+                return Constants.BLACK_TOKEN;
+            else
+                return Constants.RED_TOKEN;
+        }
     }
 }
